63d922e713e0269983c661b23acbc1e49dec56b5
SuguruOoki <oki.work0527@gmail.com>
2021-10-11 23:40:38 +0900

    [ADD]型エラーを出すところまで。

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,13 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
+    // 1~100までの乱数を生成
+    // gen_rangeメソッドは二つの数字を引数に取り、 それらの間の乱数を生成してくれます。
+    // 範囲は下限値を含み、上限値を含まないため、1と101と指定しないと1から100の範囲の数字は得られません。
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
