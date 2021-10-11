788dcc3534d6ff30519b2b4cf5197647eb92fe73
SuguruOoki <oki.work0527@gmail.com>
2021-10-11 23:49:10 +0900

    [ADD]loopを追加

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -12,27 +12,29 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        // 行の読み込みに失敗しました
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            // 行の読み込みに失敗しました
+            .expect("Failed to read line");
 
-    // 1. ユーザがエンターを押した
-    // 2. 改行文字が追加される
-    // 3. 改行文字をtrimする
-    // 4. 文字列のparseメソッドは、文字列を解析して何らかの数値にする
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        // 1. ユーザがエンターを押した
+        // 2. 改行文字が追加される
+        // 3. 改行文字をtrimする
+        // 4. 文字列のparseメソッドは、文字列を解析して何らかの数値にする
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    // 次のように予想しました: {}
-    println!("You guessed: {}", guess);
+        // 次のように予想しました: {}
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),       //小さすぎ！
-        Ordering::Greater => println!("Too big!"),      //大きすぎ！
-        Ordering::Equal => println!("You win!"),        //やったね！
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),       //小さすぎ！
+            Ordering::Greater => println!("Too big!"),      //大きすぎ！
+            Ordering::Equal => println!("You win!"),        //やったね！
+        }
     }
 }
