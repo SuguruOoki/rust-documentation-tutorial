26899729802a7604a085a5d0e0c162b8049f34c8
SuguruOoki <oki.work0527@gmail.com>
2021-10-11 23:26:49 +0900

    [ADD]最初の部分をテストするまで終了

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");          // 数を当ててごらん
+
+    println!("Please input your guess.");   // ほら、予想を入力してね
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        // 行の読み込みに失敗しました
+        .expect("Failed to read line");
+
+    // 次のように予想しました: {}
+    println!("You guessed: {}", guess);
 }
