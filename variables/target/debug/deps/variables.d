ea9d5c869d505657ad7d12420d3b3c8f571ed209
SuguruOoki <oki.work0527@gmail.com>
2021-10-12 00:17:58 +0900

    [ADD]main.rsを実装

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/suguruohki/workspace/rust-projects/variables/target/debug/deps/variables: src/main.rs
+
+/Users/suguruohki/workspace/rust-projects/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/suguruohki/workspace/rust-projects/variables/target/debug/variables: /Users/suguruohki/workspace/rust-projects/variables/src/main.rs
