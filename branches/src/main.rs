4dcce5459b9f673c6b0fb773c512cd990081e2c9
SuguruOoki <oki.work0527@gmail.com>
2021-10-13 00:55:54 +0900

    [ADD]revを使いながら展開するロジックを修正

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -28,7 +28,7 @@ fn main() {
     //     println!("again!"); // また
     // }
 
-    let a = [10, 20, 30, 40, 50];
+    // let a = [10, 20, 30, 40, 50];
     // let mut index = 0;
     // while index < a.len() {
     //     // 値は{}です
@@ -37,8 +37,13 @@ fn main() {
     //     index = index + 1;
     // }
 
-    for element in a.iter() {
-        // 値は{}です
-        println!("the value is: {}", element);
+    // for element in a.iter() {
+    //     // 値は{}です
+    //     println!("the value is: {}", element);
+    // }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
     }
+    println!("LIFTOFF!!!");
 }
