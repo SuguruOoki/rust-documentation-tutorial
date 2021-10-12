6477049b8207b07487b5c37de587236bd5a6f393
SuguruOoki <oki.work0527@gmail.com>
2021-10-13 01:40:12 +0900

    [ADD]clone()を初めて使ってみた

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
-    let mut s = String::from("hello");
-    println!("{}", s); // これは`hello, world!`と出力する
+    // let mut s = String::from("hello");
+    // println!("{}", s); // これは`hello, world!`と出力する
+    // s.push_str(", world!"); // push_str()関数は、リテラルをStringに付け加える
+    // println!("{}", s); // これは`hello, world!`と出力する
 
-    s.push_str(", world!"); // push_str()関数は、リテラルをStringに付け加える
+    // let s1 = String::from("hello");
+    // let s2 = s1;
 
-    println!("{}", s); // これは`hello, world!`と出力する
+    // // println!("{}, world!", s1);
+    // println!("{}, world!", s2);
+    // // (注釈: ムーブが起きたのは、`s1`が`std::string::String`という
+    // //     `Copy`トレイトを実装していない型だからです)
+
+    let s1 = String::from("hello");
+    // PHPの参照系のものと同じような扱いをすれば良いという理解。
+    let s2 = s1.clone();
+
+    println!("{}, {}, world!", s1, s2);
 }
