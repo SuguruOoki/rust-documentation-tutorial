3d2aec8cf2bfc24e069a9fb3f6923a18b4dbe06b
SuguruOoki <oki.work0527@gmail.com>
2021-10-13 00:37:21 +0900

    [ADD]ifの評価OK

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
     let number = 3;
 
-    if number < 5 {
-        println!("condition was true"); // 条件は真でした
-    } else {
-        println!("condition was false"); // 条件は偽でした
+    // if number < 5 {
+    //     println!("condition was true"); // 条件は真でした
+    // } else {
+    //     println!("condition was false"); // 条件は偽でした
+    // }
+
+    // この形式はPHPではtrueとなるが、Rustでは型が異なるためビルドエラーとなる。
+    // if number {
+    //     println!("number was three"); // 数値は3です
+    // }
+
+    // 比較されているため、この形式は問題なし。
+    if number != 0 {
+        println!("number was something other than zero"); // 数値は0以外の何かです
     }
 }
