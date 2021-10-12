f124bca435761002feda8c0fc519afe2cd21690f
SuguruOoki <oki.work0527@gmail.com>
2021-10-13 00:30:43 +0900

    [ADD]function for add

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -7,10 +7,10 @@
 // }
 
 fn main() {
-    another_function(5, 6);
+    let x = another_function(5, 6);
+    println!("The value of x is: {}", x);
 }
 
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
+fn another_function(x: i32, y: i32) -> i32 {
+    x + y
 }
