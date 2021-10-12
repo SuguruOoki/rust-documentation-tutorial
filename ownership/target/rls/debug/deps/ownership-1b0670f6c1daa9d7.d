525347558913f9d135dab4da4c78a79f0db91e2b
SuguruOoki <oki.work0527@gmail.com>
2021-10-13 01:20:49 +0900

    [ADD]ownershipのプロジェクトを追加

---
+++ ownership/target/rls/debug/deps/ownership-1b0670f6c1daa9d7.d
@@ -0,0 +1,5 @@
+/Users/suguruohki/workspace/rust-projects/ownership/target/rls/debug/deps/ownership-1b0670f6c1daa9d7.rmeta: src/main.rs
+
+/Users/suguruohki/workspace/rust-projects/ownership/target/rls/debug/deps/ownership-1b0670f6c1daa9d7.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/rls/debug/deps/ownership-e1328de2e127283b.d
@@ -0,0 +1,5 @@
+/Users/suguruohki/workspace/rust-projects/ownership/target/rls/debug/deps/ownership-e1328de2e127283b.rmeta: src/main.rs
+
+/Users/suguruohki/workspace/rust-projects/ownership/target/rls/debug/deps/ownership-e1328de2e127283b.d: src/main.rs
+
+src/main.rs:
